537a63e9e5238426938049f24d445ea3c5cbef12
Nandini Singh <singhnandini60810@gmail.com>
2026-03-14 18:25:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let minutes = 25;
 let seconds = 0;
-let timer;
+let timer = null;
 let sessions = 0;
 let isBreak = false;
 
@@ -8,7 +8,11 @@ const minutesDisplay = document.getElementById("minutes");
 const secondsDisplay = document.getElementById("seconds");
 const progress = document.getElementById("progress");
 const sessionsDisplay = document.getElementById("sessions");
+const modeText = document.getElementById("modeText");
 const alarm = document.getElementById("alarm");
+
+const studyInput = document.getElementById("studyInput");
+const breakInput = document.getElementById("breakInput");
 
 function updateDisplay(){
 minutesDisplay.textContent = minutes.toString().padStart(2,"0");
@@ -19,6 +23,11 @@ function startTimer(){
 
 if(timer) return;
 
+if(!isBreak){
+minutes = parseInt(studyInput.value);
+seconds = 0;
+}
+
 timer = setInterval(()=>{
 
 if(seconds === 0){
@@ -28,30 +37,38 @@ if(minutes === 0){
 alarm.play();
 
 if(!isBreak){
+
 sessions++;
 sessionsDisplay.textContent = sessions;
-minutes = 5;
+
+minutes = parseInt(breakInput.value);
+seconds = 0;
+
 isBreak = true;
+modeText.textContent = "Break Mode";
 alert("Break Time!");
 }
 else{
-minutes = 25;
+
+minutes = parseInt(studyInput.value);
+seconds = 0;
+
 isBreak = false;
+modeText.textContent = "Study Mode";
 alert("Study Time!");
 }
 
-seconds = 0;
 return;
 }
 
 minutes--;
 seconds = 59;
-}
-else{
+
+}else{
 seconds--;
 }
 
-let totalTime = isBreak ? 300 : 1500;
+let totalTime = isBreak ? breakInput.value*60 : studyInput.value*60;
 let timeLeft = minutes*60 + seconds;
 
 progress.style.width = ((totalTime-timeLeft)/totalTime)*100 + "%";
@@ -71,10 +88,11 @@ function resetTimer(){
 clearInterval(timer);
 timer = null;
 
-minutes = 25;
+minutes = parseInt(studyInput.value);
 seconds = 0;
 isBreak = false;
 
+modeText.textContent = "Study Mode";
 progress.style.width = "0%";
 
 updateDisplay();
